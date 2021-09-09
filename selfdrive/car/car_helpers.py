b9ea399955a14910ab0ca04b9b5903bd27fb7630
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-09-09 13:48:10 -0400

    forcing PQ

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -86,8 +86,8 @@ def only_toyota_left(candidate_cars):
 
 # **** for use live only ****
 def fingerprint(logcan, sendcan, has_relay):
-  fixed_fingerprint = os.environ.get('FINGERPRINT', "")
-  skip_fw_query = os.environ.get('SKIP_FW_QUERY', False)
+  fixed_fingerprint = os.environ.get('FINGERPRINT', "Generic Volkswagen PQ35/PQ46/NMS Platform Vehicle")
+  skip_fw_query = os.environ.get('SKIP_FW_QUERY', True)
 
   if has_relay and not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
