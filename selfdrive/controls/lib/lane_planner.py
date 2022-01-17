d326260dc462c4c72908f05aabf91f5bc681e004
Michal Varga <40143965+mich41v4294@users.noreply.github.com>
2022-01-17 16:10:33 +0100

    tuning

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -9,7 +9,7 @@ from selfdrive.swaglog import cloudlog
 
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
-CAMERA_OFFSET = 0.06
+CAMERA_OFFSET = -0.47
 PATH_OFFSET = 0.0
 
 
