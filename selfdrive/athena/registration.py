7b2eae53462b961b3fcdf6a9493914b806a69a88
Adam Black <18472768+AdamSBlack@users.noreply.github.com>
2022-10-11 20:34:33 +0100

    disable

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -25,6 +25,8 @@ def register(show_spinner=False) -> str:
   HardwareSerial = params.get("HardwareSerial", encoding='utf8')
   dongle_id = params.get("DongleId", encoding='utf8')
   needs_registration = None in (IMEI, HardwareSerial, dongle_id)
+  
+  return UNREGISTERED_DONGLE_ID
 
   pubkey = Path(PERSIST+"/comma/id_rsa.pub")
   if not pubkey.is_file():
